84563069ebea8a649fdad9b9c76a2d3c47165591
Hexecode <cris190402@hotmail.com>
2025-07-08 22:20:40 +0200

    Preprocesado: removido paralelismo con modin

--- InformerVanilla/data/data_loader.py
+++ InformerVanilla/data/data_loader.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
-#import pandas as pd
-import modin.pandas as pd
+import pandas as pd
+#import modin.pandas as pd
 
 import torch
 from torch.utils.data import Dataset, DataLoader
