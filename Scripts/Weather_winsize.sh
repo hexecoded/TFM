c1bbff64a6f0a23c6a593430f8b5050d1895e048
Cristhian Moya <101148642+hexecoded@users.noreply.github.com>
2025-08-01 10:44:26 +0200

    Experimentación: añadidos scripts tamaño ventana
    Scripts para slurm que ejecutan el dataset especificado para tratar de encontrar experimentalmente una heurística a la hora de escoger tamaño de ventana

---
+++ Scripts/Weather_winsize.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#SBATCH --job-name=Weather_Winsize             # Nombre del proceso
+#SBATCH --partition=dgx2                        # Cola para ejecutar
+#SBATCH --gres=gpu:1                            # Número de GPUs a usar
+#SBATCH --ntasks=1                              # Número de tareas (procesos)
+#SBATCH --cpus-per-task=32                      # Número de CPUs por tarea
+
+export PATH="/opt/anaconda/anaconda3/bin:$PATH"
+export PATH="/opt/anaconda/bin:$PATH"
+export TFHUB_CACHE_DIR="/mnt/homeGPU/hexecode/cache"
+
+eval "$(conda shell.bash hook)"
+conda activate /mnt/homeGPU/hexecode/pt23_env
+
+# Parámetros configurables
+start=3
+end=96
+dataset="WTH"
+data_path="weather.csv"
+root_path="./Datasets/Weather"
+
+# Bucle de barrido de tamaños de ventana
+for window in $(seq $start $end); do
+    echo ">>> Ejecutando con ventana: $window (all_pe_weighted)"
+    python -u run_exp.py \
+    --model informer \
+    --data $dataset \
+    --ex_name ${dataset}_all_pe_weighted_win$window \
+    --data_path $data_path \
+    --root_path $root_path \
+    --folder InformerVanilla \
+    --dropout 0.2 \
+    --itr 8 \
+    --attn full \
+    --seq_len 96 \
+    --label_len 96 \
+    --pred_len 48 \
+    --features M \
+    --enc_in 21 \
+    --dec_in 21 \
+    --c_out 21 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --time_encoding all_pe_weighted \
+    --window $window \
+    --des 'Exp'
+
+    echo ">>> Ejecutando con ventana: $window (tpe)"
+    python -u run_exp.py \
+    --model informer \
+    --data $dataset \
+    --ex_name ${dataset}_tpe_win$window \
+    --data_path $data_path \
+    --root_path $root_path \
+    --folder InformerVanilla \
+    --dropout 0.2 \
+    --itr 8 \
+    --attn full \
+    --seq_len 96 \
+    --label_len 96 \
+    --pred_len 48 \
+    --features M \
+    --enc_in 21 \
+    --dec_in 21 \
+    --c_out 21 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --time_encoding tpe \
+    --window $window \
+    --des 'Exp'
+done
